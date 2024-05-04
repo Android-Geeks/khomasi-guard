c1e9cff9b19599267f13e235a1d6004029b09c1b
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-05-04 17:24:32 +0300

    add fonts and theme

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven("https://jitpack.io")
     }
 }
 dependencyResolutionManagement {
@@ -10,8 +11,11 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io")
+        gradlePluginPortal()
     }
 }
 
-rootProject.name = "AppStarter"
+
+rootProject.name = "KhomasiGuard"
 include(":app")
