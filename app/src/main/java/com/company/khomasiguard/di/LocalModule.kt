d20a4ab7ad146b705d395386c318255bbdf05b68
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-05-05 18:28:33 +0300

    add di

---
+++ app/src/main/java/com/company/khomasiguard/di/LocalModule.kt
@@ -0,0 +1,33 @@
+package com.company.khomasiguard.di
+
+import android.app.Application
+import com.company.khomasiguard.data.repository.LocalGuardRepositoryImpl
+import com.company.khomasiguard.domain.repository.LocalGuardRepository
+import com.company.khomasiguard.domain.use_case.app_entry.AppEntryUseCases
+import com.company.khomasiguard.domain.use_case.app_entry.ReadAppEntry
+import com.company.khomasiguard.domain.use_case.app_entry.SaveIsLogin
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalModule {
+    @Provides
+    @Singleton
+    fun provideLocalGuardRepository(
+        application: Application
+    ): LocalGuardRepository = LocalGuardRepositoryImpl(context = application)
+
+    @Provides
+    @Singleton
+    fun provideUseCases(
+        localGuardRepository: LocalGuardRepository
+    ): AppEntryUseCases = AppEntryUseCases(
+        readAppEntry = ReadAppEntry(localGuardRepository),
+        saveIsLogin = SaveIsLogin(localGuardRepository)
+    )
+
+}
---
+++ app/src/main/java/com/company/khomasiguard/di/NetworkModule.kt
@@ -0,0 +1,55 @@
+package com.company.khomasiguard.di
+
+
+import com.company.khomasiguard.data.data_source.remote.RetrofitService
+import com.company.khomasiguard.data.repository.RemoteGuardRepositoryImpl
+import com.company.khomasiguard.domain.repository.RemoteGuardRepository
+import com.company.khomasiguard.util.BASE_URL
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            addInterceptor(loggingInterceptor)
+            connectTimeout(1, TimeUnit.MINUTES) // connect timeout
+            readTimeout(30, TimeUnit.SECONDS) // socket timeout
+            writeTimeout(15, TimeUnit.SECONDS) // write timeout
+        }.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideService(okHttpClient: OkHttpClient): RetrofitService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+            .create(RetrofitService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemoteGuardRepository(
+        retrofitService: RetrofitService
+    ): RemoteGuardRepository = RemoteGuardRepositoryImpl(retrofitService)
+
+}
