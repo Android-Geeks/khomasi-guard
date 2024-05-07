c5ad7ff8667cfa7403177bfd07ab367a188290f2
Ali <alialashwall14@gmail.com>
2024-05-07 06:24:11 +0300

    add extension String to Date

--- app/src/main/java/com/company/khomasiguard/util/Extensions.kt
+++ app/src/main/java/com/company/khomasiguard/util/Extensions.kt
@@ -1,2 +1,15 @@
 package com.company.khomasiguard.util
 
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+fun String.toDate(format: String = "yyyy-MM-dd"): Date? {
+    val sdf = SimpleDateFormat(format, Locale.getDefault())
+    return try {
+        sdf.parse(this)
+    } catch (ex: Exception) {
+        null
+    }
+}
+
