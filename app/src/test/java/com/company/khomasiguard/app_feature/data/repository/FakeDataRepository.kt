f57ec1af19748680a5daa263631660cf071a3f1d
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-05-08 16:08:39 +0300

    fix hilt

--- app/src/test/java/com/company/khomasiguard/app_feature/data/repository/FakeDataRepository.kt
+++ app/src/test/java/com/company/khomasiguard/app_feature/data/repository/FakeDataRepository.kt
@@ -3,8 +3,7 @@ package com.company.khomasiguard.app_feature.data.repository
 import com.company.khomasiguard.data.data_source.remote.RetrofitService
 
 class FakeDataRepository(
-    private val retrofitService: RetrofitService
-) {
 
+) {
 
 }
