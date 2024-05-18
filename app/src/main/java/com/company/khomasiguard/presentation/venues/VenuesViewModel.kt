a2b89c9a55969804f4a2b4ba37fa5d939eb0cb02
Zeinab979 <zeinabwahdan3677@gmail.com>
2024-05-18 22:26:18 +0300

    edit venues view model

--- app/src/main/java/com/company/khomasiguard/presentation/venues/VenuesViewModel.kt
+++ app/src/main/java/com/company/khomasiguard/presentation/venues/VenuesViewModel.kt
@@ -4,16 +4,13 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.company.khomasiguard.domain.DataState
-import com.company.khomasiguard.domain.model.LocalGuard
-import com.company.khomasiguard.domain.model.playground.PlaygroundsResponse
 import com.company.khomasiguard.domain.use_case.local_guard.LocalGuardUseCases
 import com.company.khomasiguard.domain.use_case.remote_guard.RemoteUseCases
 import com.company.khomasiguard.presentation.venues.components.VenuesUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -23,28 +20,14 @@ class VenuesViewModel @Inject constructor(
     private val remoteUseCases: RemoteUseCases
 ) : ViewModel()
 {
-    private val _venuesState: MutableStateFlow<DataState<PlaygroundsResponse>> =
-        MutableStateFlow(DataState.Empty)
-        val venuesState: StateFlow<DataState<PlaygroundsResponse>> = _venuesState
-
     private val _uiState: MutableStateFlow<VenuesUiState> = MutableStateFlow(VenuesUiState())
     val uiState: StateFlow<VenuesUiState> = _uiState
-    private val _localGuard = localGuardUseCases.getLocalGuard().stateIn(
-        viewModelScope, SharingStarted.WhileSubscribed(5_000), LocalGuard()
-    )
-    val localGuard: StateFlow<LocalGuard> = _localGuard
 
     fun getGuardPlaygrounds() {
         viewModelScope.launch {
-            val token =
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoidXNlcjI2QGV4YW1wbGUuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiR3VhcmQiLCJleHAiOjE3MTgzMDQ1OTksImlzcyI6IldlYkFQSURlbW8iLCJhdWQiOiJXZWJBUElEZW1vIn0.meTiTWIMG1vROhBANdYc__JLeOdTCEfbfa7ftnuCAKo"
-            val guardID = "08b37a90-831e-49cc-9ec9-f2326e705eb9"
-            // val token = _localGuard.value.token ?: ""
-            // val guardID = _localGuard.value.guardID ?: ""
-            if (token.isEmpty() || guardID.isEmpty()) {
-                Log.e("VenuesViewModel", "Token or Guard ID is missing")
-                return@launch
-            }
+            val localGuard = localGuardUseCases.getLocalGuard().first()
+            val token = localGuard.token ?: ""
+            val guardID = localGuard.guardID ?: ""
             remoteUseCases.getGuardPlaygroundsUseCase(
                 token = "Bearer $token",
                 guardID = guardID,
@@ -57,7 +40,6 @@ class VenuesViewModel @Inject constructor(
                                 playgroundName = playground.playgroundInfo.playground.name
                             )
                         }
-
                     _uiState.value = _uiState.value.copy(
                         activated = dataState.data.playgrounds.filter { playground ->
                             playground.playgroundInfo.playground.isBookable
@@ -66,7 +48,6 @@ class VenuesViewModel @Inject constructor(
                         },
                     )
                 }
-
                     is DataState.Error -> {
                         Log.e("VenuesViewModel", "Error fetching playgrounds: ${dataState.message}")
                     }
