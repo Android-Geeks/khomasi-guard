9e68b86cb8a76557fab2b25e9d84006be06fb65b
Zeinab979 <zeinabwahdan3677@gmail.com>
2024-06-21 00:23:27 +0300

    edit booking screen

--- app/src/main/java/com/company/khomasiguard/presentation/booking/BookingScreen.kt
+++ app/src/main/java/com/company/khomasiguard/presentation/booking/BookingScreen.kt
@@ -146,11 +146,29 @@ package com.company.khomasiguard.presentation.booking
 import android.content.res.Configuration
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -166,7 +184,6 @@ import com.company.khomasiguard.presentation.booking.component.Header
 import com.company.khomasiguard.presentation.components.ShortBookingCard
 import com.company.khomasiguard.theme.KhomasiGuardTheme
 import kotlinx.coroutines.flow.StateFlow
-import org.threeten.bp.LocalDate
 import org.threeten.bp.format.DateTimeFormatter
 
 @Composable
@@ -228,14 +245,16 @@ fun BookingScreen(
             ) {
                 if (uiState.guardBookings.isNotEmpty()) {
                     itemsIndexed(uiState.guardBookings) { _, item ->
-                        ShortBookingCard(
-                            bookingDetails = item.let { uiState.bookingDetails },
-                            playgroundName = "playgroundName",
-                            onClickViewBooking = {
-                                item.let { uiState.bookingDetails.bookingNumber }
-                            },
-                            onClickCall = {}
-                        )
+                        item.bookings.forEach { booking ->
+                            ShortBookingCard(
+                                bookingDetails = booking,
+                                playgroundName = item.playgroundName,
+                                onClickViewBooking = {
+                                    booking.bookingNumber
+                                },
+                                onClickCall = {}
+                            )
+                        }
                     }
                 }
             }
