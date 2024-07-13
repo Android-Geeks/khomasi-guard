7bd020c1e1868b47ea973aec307b8147c751a3d0
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-07-14 01:51:56 +0300

    change base url

--- app/src/main/java/com/company/khomasiguard/util/Constants.kt
+++ app/src/main/java/com/company/khomasiguard/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.company.khomasiguard.util
 
 //Emulator
-const val BASE_URL = "http://10.0.2.2:5000/api/"
+const val BASE_URL = "https://relieved-chicken-sterling.ngrok-free.app/api/"
 
 //Physical Device
 //const val BASE_URL = "http://localhost:5000/api/"
